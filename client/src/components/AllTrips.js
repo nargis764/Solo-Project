781d7b8b65695e324e009d17a8132319c7f06b55
nargis764 <nargis764@gmail.com>
2022-04-16 22:13:39 -0700

    Chnaged the visibilty of Edit/Delete for not loggedin users

--- client/src/components/AllTrips.js
+++ client/src/components/AllTrips.js
@@ -1,7 +1,6 @@
-import axios from 'axios'
 import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import {Link} from "react-router-dom"
-//import { Typography, Container, Grow, Grid } from '@mui/material';
 import moment from "moment";
 import {useNavigate} from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
